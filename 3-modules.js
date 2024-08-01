eb17c4e63c74bee9265c6dac31ac5c58dfeb1456
Sathi <sathi@example.com>
2024-08-01 21:58:17 +0530

    Initial commit

---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+const name = require("./4-name");
+const sayHi = require("./5-utls");
+const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
+sayHi("sansa");
+sayHi(name.john);
+sayHi(name.peter);
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,6 @@
+module.exportsitems.items = ["item1", "item2"];
+const person = {
+  name: "bob",
+};
+
+module.exportsitems.singlePerson = person;
